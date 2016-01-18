28f26813cbd54c39dbf86da52672d25a9b3ca592
Katherine Beame <katherine.beame@gmail.com>
2016-01-18 15:02:40 -0800

    an image appears on the screen

--- js/picture.js
+++ js/picture.js
@@ -1,24 +1,26 @@
 //ajascript for random photo here
 //Constructor Function to Create Image objects
-var imgName = ["bag", "banana", "boots", "chair", "cthulhu", "dragon", "pen", "scissors", "shark", "sweep", "unicorn", "usb","water-can","water-glass"];
-// var imgName = [bag, banana, boots, chair, cthulhu, dragon, pen, scissors, shark, sweep, unicorn, usb,"water-can","water-glass"];
+var imgName = ['bag', 'banana', 'boots', 'chair', 'cthulhu', 'dragon', 'pen', 'scissors', 'shark', 'sweep', 'unicorn', 'usb','water-can','water-glass'];
+// var imgName = [bag, banana, boots, chair, cthulhu, dragon, pen, scissors, shark, sweep, unicorn, usb,'water-can","water-glass"];
 
 var images = [
-  ["bag", "img/bag.jpg"],
-  ["banana", "img/banana.jpg"],
-  ["boots", "img/boots.jpg"],
-  ["chair", "img/chair.jpg"],
-  ["cthulhu", "img/cthulhu.jpg"],
-  ["dragon", "img/dragon.jpg"],
-  ["pen", "img/pen.jpg"],
-  ["scissors", "img/scissors.jpg"],
-  ["shark", "img/shark.jpg"],
-  ["sweep", "img/sweep.jpg"],
-  ["unicorn", "img/unicorn.jpg"],
-  ["usb", "img/usb.jpg"],
-  ["water-can", "img/water-can.jpg"],
-  ["water-glass", "img/water-glass.jpg"]
+  ['bag', 'img/bag.jpg'],
+  ['banana', 'img/banana.jpg'],
+  ['boots', 'img/boots.jpg'],
+  ['chair', 'img/chair.jpg'],
+  ['cthulhu', 'img/cthulhu.jpg'],
+  ['dragon', 'img/dragon.jpg'],
+  ['pen', 'img/pen.jpg'],
+  ['scissors', 'img/scissors.jpg'],
+  ['shark', 'img/shark.jpg'],
+  ['sweep', 'img/sweep.jpg'],
+  ['unicorn', 'img/unicorn.jpg'],
+  ['usb', 'img/usb.jpg'],
+  ['water-can', 'img/water-can.jpg'],
+  ['water-glass', 'img/water-glass.jpg']
 ];
+
+var imageObject = [];
 
 
 var Product = function (productName, filePath) {
@@ -27,10 +29,35 @@ var Product = function (productName, filePath) {
   this.clicksRecieved = 0;
 }
 
-// var  createObject = function() {
-//   for (var i = 0; i < images.length; i++) {
-//     var imgName[i] = new Product(images[i][0], images[i][1]);
-//     // console.log(imgName[i].Product);
-//   }
+var  createObject = function() {
+  for (var i = 0; i < images.length; i++) {
+    imageObject.push(new Product(images[i][0], images[i][1]));
+  }
+}
+createObject();
+//create a random number
+function getRandomNum() {
+    return Math.floor(Math.random() * (images.length));
+}
+//render an image
+// function renderImageOne() {
+//     getRandomNum();
+//     createObject();
+//attach the selected random image to the image one tag
+//     var imgOne = document.getElementById('imgOne');
+//     var section = document.getElementsByClassName('pick')
+//     imgOne.textContent = imageObject[getRandomNum()][1];
+//     section.appendChild = (imgOne);
+//     document.appendChild =(section);
 // }
-// createObject();
+//
+// renderImageOne();
+
+var imgOne = document.getElementById('imgOne');
+// var img1 = document.createElement('img');
+imgOne.innerHTML = "<img src=" + images[1][1] + ">";
+
+
+
+// img1.textContent = images[0][1];
+// imgOne.appendChild(img1);
